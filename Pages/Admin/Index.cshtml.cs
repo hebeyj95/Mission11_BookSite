1f92dcb54921e466bed10195df79168b991cd5dd
hebeyj95 <62028714+hebeyj95@users.noreply.github.com>
2022-03-23 14:25:14 -0600

    Log in/log out

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookSite2._0.Pages.Admin;
 
 namespace BookSite2._0.Pages.Admin
 {
